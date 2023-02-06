26d3b12e1f07596e7e6a84160755e999078b23df
LeoDeveloper1994 <leodev1994@gmail.com>
2023-02-05 21:39:24 -0600

    change github account

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='dashboard-container'></div>
+    <div className='dashboard-container'>
+      Contenido del dashboard
+    </div>
   )
 }
 
